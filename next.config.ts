dbd82345691d9a40f0454a277d118f805fab5db4
Bolnus <you@example.com>
2026-06-01 18:50:07 +0300

    fix: Secrets in .env

--- next.config.ts
+++ next.config.ts
@@ -3,52 +3,20 @@ import createNextIntlPlugin from "next-intl/plugin";
 
 const withNextIntl = createNextIntlPlugin("./src/FSD/shared/lib/i18n/request.ts");
 
-const REMOTE_IMAGE_PATHNAME = "/**";
-
-const DEV_HOST_MIKE_PC = "mike-pc.local";
+const DEV_HOSTNAMES = process.env.DEV_HOSTNAMES?.split(",") || [];
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "http",
-        hostname: "localhost",
-        port: "3001",
-        pathname: REMOTE_IMAGE_PATHNAME
-      },
-      {
-        protocol: "http",
-        hostname: DEV_HOST_MIKE_PC,
-        port: "3001",
-        pathname: REMOTE_IMAGE_PATHNAME
-      },
-      {
-        protocol: "http",
-        hostname: "localhost",
-        port: "",
-        pathname: REMOTE_IMAGE_PATHNAME
-      },
-      {
-        protocol: "http",
-        hostname: DEV_HOST_MIKE_PC,
-        port: "",
-        pathname: REMOTE_IMAGE_PATHNAME
-      },
-      {
-        hostname: "recipes-rho-ten.vercel.app",
-        port: "",
-        pathname: REMOTE_IMAGE_PATHNAME
-      },
-      {
-        protocol: "https",
-        hostname: "gallery-backend-teal.vercel.app",
-        pathname: REMOTE_IMAGE_PATHNAME
-      }
-    ],
+    remotePatterns: DEV_HOSTNAMES.map((devHost) => ({
+      protocol: "http",
+      hostname: "localhost",
+      port: "*",
+      pathname: devHost
+    })),
     qualities: [50, 100],
     dangerouslyAllowLocalIP: true
   },
-  allowedDevOrigins: [DEV_HOST_MIKE_PC, "mike-laptop.local"],
+  allowedDevOrigins: DEV_HOSTNAMES,
   staticPageGenerationTimeout: 120,
   rewrites() {
     const apiOrigin = process.env.DOTS_API_ORIGIN ?? "http://127.0.0.1:3030";
